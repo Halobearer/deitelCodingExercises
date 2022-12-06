364e4c7447e3db6733d62803c0dc6f1cc53c4681
Damian <ndebuezedamian@gmail.com>
2022-12-06 12:05:12 +0100

    Initial commit

---
+++ src/ChapterSix/MathClassAndArgumentPromotion.java
@@ -0,0 +1,11 @@
+package ChapterSix;
+
+public class MathClassAndArgumentPromotion {
+    public static void main(String[] args) {
+
+        int number = 5;
+        int digit = 10;
+        System.out.println(Math.sqrt(digit));
+        System.out.println(number);
+    }
+}
